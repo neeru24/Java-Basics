f652177e61a27560137c756d9f7b2f0b11ee9b80
Neeru <161798182+neeru24@users.noreply.github.com>
2024-07-23 21:42:10 +0530

    Create Arithmetic.java

---
+++ Arithmetic.java
@@ -0,0 +1,21 @@
+public class ArithmeticOperations {
+    public static void main(String[] args) {
+        // Define two integer variables
+        int a = 10;
+        int b = 5;
+        
+        // Perform arithmetic operations
+        int sum = a + b;
+        int difference = a - b;
+        int product = a * b;
+        int quotient = a / b;
+        int remainder = a % b;
+        
+        // Print results
+        System.out.println("Sum: " + sum);
+        System.out.println("Difference: " + difference);
+        System.out.println("Product: " + product);
+        System.out.println("Quotient: " + quotient);
+        System.out.println("Remainder: " + remainder);
+    }
+}
